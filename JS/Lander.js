2df6d58f56b6be2bae85f5414df1768f81d35b76
Ben Sharafian <benms64@gmail.com>
2015-02-14 15:20:42 -0800

    FUCK JAVASCRIPT

--- JS/Lander.js
+++ JS/Lander.js
@@ -3,26 +3,32 @@ var Lander = function(ground, scene, camera, keys) {
 	this.GRAVITY = -0.1;
 
 	this.geometry = new THREE.BoxGeometry(6, 5, 6);
-	this.material = new THREE.MeshLambertMaterial({color: 0xffffff});
+	this.material = new THREE.MeshBasicMaterial({color: 0xffffff});
 	this.object = new THREE.Mesh(this.geometry, this.material);
+	console.log(this.object);
 
 	this.camera = camera;
 	this.keys = keys;
 	this.ground = ground;
 
-	this.object.position.y = 10;
-	this.camera.position.y = 10;
+	this.object.position.y = 0;
+	this.camera.position.y = 0;
 	this.camera.position.z = 10;
 
-	this.vx = 0;
-	this.vy = 0;
-	this.vz = 0;
+	this.vx = 0.0;
+	this.vy = 0.0;
+	this.vz = 0.0;
 	
-	this.rx = 0;
-	this.ry = 0;
-	this.rz = 0;
+	this.rx = 0.0;
+	this.ry = 0.0;
+	this.rz = 0.0;
 
-	scene.add(this.object);
+	var geometry = new THREE.BoxGeometry(6, 5, 6);
+	var material = new THREE.MeshBasicMaterial({color: 0xffffff});
+	var object = new THREE.Mesh(geometry, material);
+	scene.add(object);
+	
+	this.object = object;
 }
 
 Lander.prototype.applyForce = function(x, y, z) {
@@ -40,16 +46,17 @@ Lander.prototype.applyRot = function(x, y, z) {
 
 Lander.prototype.move = function(dt) {
 	
-	[ this.object, this.camera ].forEach(function(elem, i, arr) {
+	var lander = this;
+	[ lander.object, lander.camera ].forEach(function(elem, i, arr) {
 
-/*		elem.position.x += this.vx * dt;	
-		elem.position.y += this.vy * dt;	
-		elem.position.z += this.vz * dt;*/
+		elem.position.x += lander.vx * dt;	
+		elem.position.y += lander.vy * dt;	
+		elem.position.z += lander.vz * dt;
 
 	});
-	this.object.rotation.x += this.vx * dt;	
-	this.object.rotation.y += this.vy * dt;	
-	this.object.rotation.z += this.vz * dt;	
+	this.object.rotation.x += this.rx * dt;	
+	this.object.rotation.y += this.ry * dt;	
+	this.object.rotation.z += this.rz * dt;	
 }
 
 /* keycodes */
@@ -60,6 +67,8 @@ var D = 68;
 var SPACE = 32;
 
 Lander.prototype.update = function(dt) {
+
+	if (isNaN(dt)) return;
 
 	if (this.keys[W]) this.applyRot(0.1 * dt, 0, 0);	
 	if (this.keys[A]) this.applyRot(0, 0, 0.1 * dt);	
--- JS/game.js
+++ JS/game.js
@@ -24,11 +24,15 @@ renderer.setSize( WIDTH, HEIGHT);
 
 function init () {
     var light = new THREE.PointLight( 0xffffff, 1, 200);
-    light.position.set( -50, 150, -50 );
+    light.position.set( -50, 50, 50 );
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, WIDTH/HEIGHT, .1, 1000);
     camera.position.z = 5;
     scene.add( light );
+/*	var geometry = new THREE.BoxGeometry(6, 5, 6);
+	var material = new THREE.MeshBasicMaterial({color: 0xffffff});
+	var object = new THREE.Mesh(geometry, material);
+	scene.add(object);*/
     landScape = new LandScape(scene);
     lander = new Lander( landScape, scene, camera, KEYS );
 }
