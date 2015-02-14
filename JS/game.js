4a77b8fe9524ebc7721c2cdfb8536e2a051525d3
etliu <eric.t.liu15@gmail.com>
2015-02-14 13:20:30 -0800

    added init and main

--- JS/game.js
+++ JS/game.js
@@ -1,31 +1,44 @@
 var WIDTH = 640;
 var HEIGHT = 480;
 
-var scene = new THREE.Scene();
-var camera =  new THREE.PerspectiveCamera(75, WIDTH/HEIGHT, .1, 1000);
+var scene;
+var camera;
 
 var lander;
 var landScape;
+
+var now;
+var last;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( WIDTH, HEIGHT);
 
 window.onload = function () {
     document.body.appendChild( renderer.domElement);
-    render();
-}
-
-function main () {
-    lander.update();
-    render();
+init();
+main();
 }
 
 function init () {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, WIDTH/HEIGHT, .1, 1000);
     lander = new Lander( scene, camera );
     landScape = new LandScape(scene);
 }
 
-function render() {
+function main() {
+    now = Date.now();
+    dt = (now-last) / 1000;
+    last = Date.now();
+    
+    lander.update(dt);
+    
     requestAnimationFrame( render );
 	renderer.render( scene, camera );
+}
+
+window.onload = function () {
+    document.body.appendChild( renderer.domElement);
+    init();
+    main();
 }
